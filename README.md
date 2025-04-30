fd34738819e11e7848cc1d91513dddaf1532d3b5
Rocket_GR <georgiiromanishko@gmail.com>
2025-04-30 09:20:53 +0200

    feat: queue

--- README.md
+++ README.md
@@ -316,3 +316,31 @@ int main() {
     return 0;
 }
 ```
+
+## Queue  FIFO
+
+```c++
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+int main() {
+    queue<int> q;
+
+    q.push(2);
+    q.push(3);
+    cout << q.front() << endl; //2
+
+    q.pop();
+    cout << q.front() << endl; //3
+
+    q.pop();
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    cout << q.front() << endl; //4
+
+    return 0;
+}
+```
--- main.cpp
+++ main.cpp
@@ -1,75 +1,23 @@
 #include <iostream>
-#include <stack>
+#include <queue>
 using namespace std;
 
-bool isOpen(char& ch) {
-    return ch == '(' || ch == '[' || ch == '{';
-}
-
-bool isPair(char& top, char& ch) 
-{
-    return top == '(' && ch == ')' || top == '[' && ch == ']' || top == '{' && ch == '}';
-}
-
-bool isBalanced(string& str) 
-{
-    if(str == "") {
-        return true;
-    }
-
-    stack<char> st;
-    int result = true;
-
-    for (size_t i = 0; i < str.size(); ++i)
-    {
-        if(isOpen(str[i])) 
-        {
-            st.push(str[i]);
-
-            continue;
-        }
-
-        if(st.empty()) {
-            result = false;
-
-            break;
-        }
-
-        if(isPair(st.top(), str[i])) {
-            st.pop();
-
-            continue;
-        }
-
-        result = false;
-        break;
-    }
-
-    if(!st.empty()) {
-        result = false;
-    }
-    
-    return result;
-}
 
 int main() {
-    string str1 = "()({[]}())"; //true 1
-    string str2 = "{}"; //true 1
-    string str3 = "}{"; //false 0
-    string str4 = ""; //true 1
-    string str5 = "(()"; //false 0
-    string str6 = "())"; //false 0
-    string str7 = "[(])"; //false 0
-    string str8 = "}"; //false 0
+    queue<int> q;
 
-    cout << isBalanced(str1) << endl;
-    cout << isBalanced(str2) << endl;
-    cout << isBalanced(str3) << endl;
-    cout << isBalanced(str4) << endl;
-    cout << isBalanced(str5) << endl;
-    cout << isBalanced(str6) << endl;
-    cout << isBalanced(str7) << endl;
-    cout << isBalanced(str8) << endl;
+    q.push(2);
+    q.push(3);
+    cout << q.front() << endl; //2
+
+    q.pop();
+    cout << q.front() << endl; //3
+
+    q.pop();
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    cout << q.front() << endl; //4
 
     return 0;
 }
